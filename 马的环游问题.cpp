b9b75f5b3ce0ccee5fc9527101b9221a7bcc9e1b
chknight <heatch@163.com>
2014-12-14 13:53:22 +0800

    Update 马的环游问题.cpp

--- 马的环游问题.cpp
+++ 马的环游问题.cpp
@@ -1,5 +1,6 @@
 #include<list>
 #include<cstdio>
+#include<iostream>
 using namespace std;
 void move_3up_1right(int&id, bool &visited) {
 	id -= 15;
@@ -18,11 +19,11 @@ void move__1up_3left(int&id, bool&visited) {
 	visited = true;
 }
 void move_1down_3right(int&id, bool&visited) {
-	id += 6;
+	id += 10;
 	visited = true;
 }
 void move_1down_3left(int&id, bool&visited) {
-	id += 10;
+	id += 6;
 	visited = true;
 }
 void move_3down_1right(int&id, bool&visited) {
@@ -46,10 +47,10 @@ bool check__1up_3left(int id, bool visited) {
 	return (!visited)&&(id - 10 > 0);
 }
 bool check_1down_3right(int id, bool visited) {
-	return (!visited)&&(id + 6 < 65);
+	return (!visited)&&(id + 10 < 65);
 }
 bool check_1down_3left(int id, bool visited) {
-	return (!visited)&&id + 10 < 65;
+	return (!visited)&&id + 6 < 65;
 }
 bool check_3down_1right(int id ,bool visited) {
 	return (!visited)&&id + 17 < 65;
@@ -62,24 +63,24 @@ struct node
 	bool visited = false;
 	int number;
 	bool up3_right1 = false;
-	bool up3_left = false;
+	bool up3_left1 = false;
 	bool up1_right3 = false;
 	bool up1_left3 = false;
 	bool down1_right3 = false;
 	bool down1_left3 = false;
-	bool down3_left3 = false;
-	bool down3_right3 = false;
+	bool down3_left1 = false;
+	bool down3_right1 = false;
 };
 void clear(node&block) {
 	block.visited = false;
 	block.up3_right1 = false;
-	block.up3_left = false;
+	block.up3_left1 = false;
 	block.up1_right3 = false;
 	block.up1_left3 = false;
 	block.down1_right3 = false;
 	block.down1_left3 = false;
-	block.down3_left3 = false;
-	block.down3_right3 = false;
+	block.down3_left1 = false;
+	block.down3_right1 = false;
 }
 void print(list<node>blocks) {
 	while (!blocks.empty()) {
@@ -89,51 +90,93 @@ void print(list<node>blocks) {
 	printf("\n");
 }
 void horse(int n, node*block, int step, list<node>blocks) {
+	bool visit = block[n].visited;
+	block[n].visited = true;
+	if (!visit)
+ 	    blocks.push_back(block[n]);
 	if (step == 63) {
 		print(blocks);
 		return;
 	}
-	block[n].visited = true;
-	blocks.push_back(block[n]);
-	if (check_1down_3left(n, block[n].visited)) {
-		move_1down_3left(n, block[n].visited);
+	int temp = n;
+	if (check_1down_3left(n, block[n].down1_left3)) {
+		move_1down_3left(n,block[n].down1_left3);
+		if (!block[temp].visited) {
+		move_1down_3left(n, block[n].down1_left3);
 		horse(n, block, step + 1, blocks);
+	    } else {
+	    	horse(n, block, step, blocks);
+	    }
 	}
-	else if (check_1down_3right(n,block[n].visited)) {
-		move_1down_3right(n, block[n].visited);
+	else if (check_1down_3right(n,block[n].down1_right3)) {
+		move_1down_3right(temp, block[n].down1_right3);
+		if (!block[temp].visited) {
+		move_1down_3right(n, block[n].down1_right3);
 		horse(n, block, step + 1, blocks);
+	    } else {
+	    	horse(n, block, step, blocks);
+	    }
 	}
-	else if (check_1up_3right(n, block[n].visited)) {
-		move_1up_3right(n, block[n].visited);
+	else if (check_1up_3right(n, block[n].up1_right3)) {
+		move_1up_3right(temp, block[n].up1_right3);
+		if (!block[temp].visited) {
+		move_1up_3right(n, block[n].up1_right3);
 		horse(n, block, step + 1, blocks);
+	    } else {
+	    	horse(n, block, step, blocks);
+	    }
 	}
-	else if (check__1up_3left(n, block[n].visited)) {
-		move__1up_3left(n, block[n].visited);
+	else if (check__1up_3left(n, block[n].up1_left3)) {
+		move__1up_3left(temp, block[n].up1_left3);
+		if (!block[temp].visited) {
+		move__1up_3left(n, block[n].up1_left3);
 		horse(n, block, step + 1, blocks);
+	    } else {
+	    	horse(n, block, step, blocks);
+	    }
 	}
-	else if (check_3down_1left(n, block[n].visited)) {
-		move_3down_1left(n, block[n].visited);
+	else if (check_3down_1left(n, block[n].down3_left1)) {
+		move_3down_1left(temp, block[n].down3_left1);
+		if (!block[temp].visited) {
+		move_3down_1left(n, block[n].down3_left1);
 		horse(n, block, step + 1, blocks);
+	    } else {
+	    	horse(n, block, step, blocks);
+	    }
 	}
-	else if (check_3down_1right(n, block[n].visited)) {
-		move_3down_1right(n, block[n].visited);
+	else if (check_3down_1right(n, block[n].down3_right1)) {
+		move_3down_1right(temp, block[n].down3_right1);
+		if (!block[temp].visited) {
+		move_3down_1right(n, block[n].down3_right1);
 		horse(n, block, step + 1, blocks);
+	    } else {
+	    	horse(n, block, step, blocks);
+	    }
 	}
-	else if (check_3up_1left(n, block[n].visited)) {
-		move_3up_1left(n, block[n].visited);
+	else if (check_3up_1left(n, block[n].up3_left1)) {
+		move_3up_1left(temp, block[n].down3_left1);
+		if (!block[temp].visited) {
+		move_3up_1left(n, block[n].up3_left1);
 		horse(n, block, step + 1, blocks);
+	    } else {
+	    	horse(n, block, step, blocks);
+	    }
 	}
-	else if (check_3up_1right(n, block[n].visited)) {
-		move_3up_1right(n, block[n].visited);
+	else if (check_3up_1right(n, block[n].up3_right1)) {
+		move_3up_1right(temp, block[n].up3_right1);
+		if  (!block[temp].visited) {
+		move_3up_1right(n, block[n].up3_right1);
 		horse(n, block, step + 1, blocks);
+	    } else {
+	    	horse(n, block, step, blocks);
+	    }
 	}
 	else {
 		clear(block[n]);
-		if (!blocks.empty()) {
-			blocks.pop_back();
-			horse(blocks.back().number, block, step - 1, blocks);
-		}
+		blocks.pop_back();
+		horse(blocks.back().number, block, step - 1, blocks);
 	}
+	return;
 }
 int main() {
 	int n = 1;
@@ -143,7 +186,7 @@ int main() {
 		block[i].number = i;
 	}
 	while (n != -1) {
-		scanf_s("%d", &n);
+		cin >> n;
 		if (n == -1)
 			break;
 		list<node>blocks;
